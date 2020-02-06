33a81eecc5c71d1406161b0e4482d18a43722ac0
kaprada <kevin.alexander.prada@gmail.com>
2020-02-06 22:55:51 +0000

    fix 100 and add 101

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,14 +18,14 @@ int main(void)
 		for (j = i + 1; j <= '9'; j++)
 		{
 			if (i != j && j != i)
-			{	
+			{
 				putchar(i);
 				putchar(j);
 				if (i != '8' || j != '9')
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print 0 to 99
+ *
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n, m, l;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (m = n + 1; m <= '9'; m++)
+		{
+			for (l = m + 1; l <= '9'; l++)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != '7' || m != '8' || l != '9')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
