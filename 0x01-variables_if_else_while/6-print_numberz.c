c500831c75ef1207489542cacc22543d59e3e49b
kaprada <kevin.alexander.prada@gmail.com>
2020-02-06 21:03:29 +0000

    added file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print numbers 0 to 9
+ *
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
