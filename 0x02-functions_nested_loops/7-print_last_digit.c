2a2a34019e2ccba16d87ba2b15a9d6c3b1ac1ed9
Kevin Prada <47800151+kaprada@users.noreply.github.com>
2020-02-11 13:26:19 -0800

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,24 +2,25 @@
 
 /**
 *print_last_digit - prints the last digit of a number.
-*@int: integer
+*@num: integer
 *
 *Return: The value of the las digit
 */
 
-int print_last_digit(int)
+int print_last_digit(int num)
 {
-	int last_dig
+	int last_dig;
 
-	if (last_dig < 0)
-  {
-		last_dig = (int % 10) * -1;
+	if (num < 0)
+	{
+		last_dig = (num % 10) * -1;
 	}
-
 	else
 	{
-		last_dig = int % 10;
+		last_dig = num % 10;
 	}
 	_putchar (last_dig + '0');
+
 	return (last_dig);
+
 }
