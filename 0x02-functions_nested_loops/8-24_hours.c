e46d9ea6b2894b1210d72dad32a005e259312382
Kevin Prada <47800151+kaprada@users.noreply.github.com>
2020-02-11 13:29:58 -0800

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,31 @@
+#include "holberton.h"
 
+/**
+*jack_bauer - prints every minute of the day
+* starting from 00:00 to 23:59.
+*
+*Return: Print every minute from 00:00 to 23:49
+*/
+
+void jack_bauer(void)
+{
+	int hours, min;
+
+	hours = 0;
+
+	while (hours <= 23)
+	{
+		min = 0;
+		while (min <= 59)
+		{
+			_putchar(hours / 10 + '0');
+			_putchar(hours % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
+			min++;
+		}
+		hours++;
+	}
+}
