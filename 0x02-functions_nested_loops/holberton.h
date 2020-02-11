6b2ac946d84fb984898aac1dcfa0c869448fac9f
Kevin Prada <47800151+kaprada@users.noreply.github.com>
2020-02-11 13:45:02 -0800

    Update holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -11,7 +11,7 @@ int _abs(int num);
 int print_last_digit(int num);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int i, int j);
 void print_to_98(int n);
 void print_times_table(int n);
 
