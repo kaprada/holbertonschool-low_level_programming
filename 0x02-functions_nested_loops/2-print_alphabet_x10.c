be62da6eb277fd96f5924b1a83bbe03216e8469f
kaprada <kevin.alexander.prada@gmail.com>
2020-02-11 17:16:12 +0000

    fix file 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - print alphabet using _putchar 10 times
+ * print_alphabet_x10 - print alphabet using _putchar 10 times
  *
  *
  * Return: success 0.
@@ -14,9 +14,9 @@ void print_alphabet_x10(void)
 	char alphabet = 'a';
 	int i = 0;
 
-	for (i = 0;i < 10;i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (alphabet = 'a';alphabet <= 'z'; alphabet++)
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			_putchar(alphabet);
 		}
