62d007fefb4556ca3d5025a0ff5e56ec0342134c
Kevin Prada <47800151+kaprada@users.noreply.github.com>
2020-02-11 13:44:04 -0800

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+*add - add two integers and returns the result.
+*@a: int value
+*@b: int value
+*Return: Results of add tow integers
+*/
+
+int add(int a, int b)
+{https://github.com/kaprada/holbertonschool-low_level_programming
+	int add;
+
+	res = a + b;
+
+	return (add);
+
+
+}
